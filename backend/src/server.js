f25a20b0109450cc6484b48c1cf44eaf1b4351df
Blake332 <darrelblake39@gmail.com>
2025-12-24 21:36:50 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import contactRoutes from "../routes/contactRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
@@ -25,4 +25,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
